95b6c33ac1b44b0e027ffbe6a6d65a6b1ffce756
Maksim Gudkov <39350729+clockwo@users.noreply.github.com>
2022-06-13 15:29:02 +0200

    refactoring
    Changed the variables and added comments for each step

--- wgTestTask1/main.cpp
+++ wgTestTask1/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace::std;
 
 int main() {
 
     std::string path {"/Users/maxim/CLionProjects/wgTestTask1/file.txt"}; //path to file.txt
     std::ifstream obj;
+
     obj.open(path);
     if (obj.is_open()) {
-        std::string str;
-        std::string newStr;
-        size_t posSemi {};
-        int tempNumber {};
+        //init strings
+        std::string str, newStr;
         while (!obj.eof()) {
-            posSemi = 0;
-            str = "";
+            //set variables to no data
+            str.clear();
+            //get string from file.txt
             getline(obj,str);
-            //find ';'
             if (str.length() != 0) {
-                posSemi = str.find(";");
+                //init and find ';'
+                size_t posSemi = str.find(';');
+                //cut string after semicolon
                 std::string tempString {str, posSemi + 1, str.size()};
-                tempNumber = stoi(tempString);
-                //find number 2
+                //init and converting a string value into an int value
+                int tempNumber = stoi(tempString);
+                //find the second runner
                 if (tempNumber == 2) {
                    newStr = str.substr(0, posSemi);
                 }
             }
         }
-        cout << newStr << endl;
+        std::cout << newStr << std::endl;
     } else {
         std::cout << "0" << std::endl;
     }
