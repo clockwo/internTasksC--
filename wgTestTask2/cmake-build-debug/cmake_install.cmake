47cb9ac6719d89993c06c77fd4e64ec1288408de
Maksim Gudkov <39350729+clockwo@users.noreply.github.com>
2022-06-13 16:13:36 +0200

    Refactoring

--- wgTestTask2/cmake-build-debug/cmake_install.cmake
+++ wgTestTask2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/maxim/CLionProjects/wgTestTask2
+# Install script for directory: /Users/maxim/Desktop/internTasks/wgTestTask2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/maxim/CLionProjects/wgTestTask2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/maxim/Desktop/internTasks/wgTestTask2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- wgTestTask2/main.cpp
+++ wgTestTask2/main.cpp
@@ -1,50 +1,60 @@
 #include <iostream>
 #include <vector>
-using namespace::std;
 
 int findDig(int x);
-
 bool findIncludeDig(int x, int n);
+std::vector <int> createVector(const int &n);
+
+void printFirstSequence(const std::vector <int> &arr, const int &x);
 
 int main() {
     //variables
-    int x {3}, n {20}, countRounds{}; //change here n of sequence
+    int x {3}, n {20} , countRounds{}, count {}; //change here n of sequence
+    //setArrays
+    std::vector <int> arr = createVector(n);
+    std::vector <int> arrSecond;
 
-    vector <int> arr;
-    vector <int> arr2;
-    int count {};
-    //creat massive
-    for (size_t i {1}; i <= n; i++) {
-        arr.push_back(i);
-    }
-    for (auto const &ar : arr) {
-        cout << ar << ' ';
-    }
-    cout << "Digt number: " << findDig(x) << " | X: " << x << endl;
-    cout << endl;
+    printFirstSequence(arr, x);
+
     while (x != 0) {
         int digNumber {findDig(x)};
         for (int i{}; i < arr.size(); i++) {
             if (arr.at(i) % x != 0 && !findIncludeDig(digNumber, arr.at(i) )) {
-                arr2.push_back(arr.at(i));
+                arrSecond.push_back(arr.at(i));
             } else {
                 count++;
             }
         }
         x = count;
         count = 0;
-        for (auto const &ar : arr2) {
-            cout << ar << ' ';
+        for (auto const &ar : arrSecond) {
+            std::cout << ar << ' ';
         }
-        cout << "Digt number: " << findDig(x) << " | X: " << x << endl;
-        cout << endl;
+        std::cout << "Digit number: " << findDig(x) << " | X: " << x << std::endl;
+        std::cout << std::endl;
         arr.clear();
-        arr = arr2;
-        arr2.clear();
+        arr = arrSecond;
+        arrSecond.clear();
         countRounds++;
     }
     std::cout << "Rounds: " << countRounds << std::endl;
     return 0;
+}
+
+void printFirstSequence(const std::vector <int> &arr, const int &x){
+    for (auto const &ar : arr) {
+        std::cout << ar << ' ';
+    }
+    std::cout << "Digit number: " << findDig(x) << " | X: " << x << std::endl;
+    std::cout << std::endl;
+}
+
+std::vector <int> createVector(const int &n) {
+    std::vector <int> tempVector;
+    for (int i {1}; i <= n; i++) {
+        tempVector.push_back(i);
+    }
+    return tempVector;
 }
 
 int findDig(int x) {
@@ -60,9 +70,8 @@ int findDig(int x) {
 }
 
 bool findIncludeDig(int x, int n) {
-    int k {};
     while (n != 0) {
-        k = n % 10;
+        int k {n % 10};
         if (k == x) {
             return true;
         }
